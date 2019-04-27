d9e575f927cff3f10a855807ba124639f9f0504e
Damien <damien@damienvalade.fr>
2019-04-27 21:28:01 +0200

    add css & index.php
    add css & index.php

---
+++ htdocs/index.php
@@ -0,0 +1,18 @@
+<?php
+
+define('ROOT', dirname(__DIR__));
+
+require_once ROOT . '/vendor/autoload.php';
+require_once ROOT . '/src/Controller/Controller.php';
+require_once ROOT . '/src/Controller/FrontController.php';
+
+
+use App\Controller\FrontController;
+use Tracy\Debugger;
+
+Debugger::enable();
+Debugger::$strictMode = true;
+
+$test = new FrontController();
+$test->__construct();
+

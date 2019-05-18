c60fa84e541f86b134c72f7919b5dac64a15fb81
damienvalade <damien@damienvalade.fr>
2019-05-18 18:58:45 +0200

    Add Article board
    Add Article board

--- src/Controller/AdminController/ArticlesController.php
+++ src/Controller/AdminController/ArticlesController.php
@@ -4,12 +4,24 @@
 namespace App\Controller\AdminController;
 
 
+use Core\Model\Model;
+
 class ArticlesController
 {
 
+    protected $data;
+    protected $database;
+
+    public function __construct()
+    {
+        $this->database = new Model();
+    }
+
     public function indexAction()
     {
-       // TODO write function
+        $this->data = $this->database->read('articles');
+
+        return ['articles' => $this->data] ;
     }
 
 }

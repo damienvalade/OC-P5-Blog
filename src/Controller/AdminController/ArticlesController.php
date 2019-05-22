abb63c0d33c57de9077e98ebfe0297e853ec3e5e
damienvalade <damien@damienvalade.fr>
2019-05-23 00:22:13 +0200

    update dashboard User
    update dashboard User

--- src/Controller/AdminController/ArticlesController.php
+++ src/Controller/AdminController/ArticlesController.php
@@ -6,18 +6,21 @@ namespace App\Controller\AdminController;
 
 use App\Model\AdminModel\ArticlesModel;
 use Core\Controller\FrontController;
+use Core\Controller\Session\Session;
 
 
 class ArticlesController extends FrontController
 {
 
     protected $data;
+    protected $session;
     protected $data2;
     protected $database;
 
     public function __construct()
     {
         $this->database = new ArticlesModel();
+        $this->session = new Session();
     }
 
     public function indexAction()
@@ -29,7 +32,9 @@ class ArticlesController extends FrontController
 
     public function updateAction(){
 
-        $this->data = $this->database->read('articles', '1', 'id', true);
+        $id = (int)$_GET['id'];
+
+        $this->data = $this->database->read('articles', $id, 'id', true);
         $this->data2 = $this->database->read('categories');
 
         return ['articles' => $this->data,
@@ -37,7 +42,36 @@ class ArticlesController extends FrontController
     }
 
     public function createAction(){
-        // TODO
+
+        $this->data2 = $this->database->read('categories');
+
+        if (!empty($_POST)) {
+
+            if (!empty($_POST['inputName']) && !empty($_POST['inputChapo'])
+                && !empty($_POST['inputType']) && !empty($_POST['inputContenue'])) {
+
+                $inputName = $_POST['inputName'];
+                $inputChapo = $_POST['inputChapo'];
+                $inputType = $_POST['inputType'];
+                $inputContenue = $_POST['inputContenue'];
+
+                    $filename = $this->upload('photoprofil');
+
+                        $data = [
+                            'nomArticle' => $inputName,
+                            'chapoArticle' => $inputChapo,
+                            'auteurArticle' => $this->session->userName(),
+                            'id_categories' => $inputType,
+                            'contenueArticle' => $inputContenue,
+                            'image' => 'img\\\\photoprofil\\\\' . $filename,
+                        ];
+
+                        $this->database->create('articles', $data);
+
+                        $this->session->setValidate('inscription', 'Article mis en ligne');
+            }
+        }
+        return ['types' => $this->data2] ;
     }
 
 }

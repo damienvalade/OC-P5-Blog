74c4fb6964fbfe99a17fcae0cf23aaca8d1d035f
damienvalade <damien@damienvalade.fr>
2019-06-02 01:43:25 +0200

    test superglobal post
    test superglobal post

--- bin/Controller/FrontController.php
+++ bin/Controller/FrontController.php
@@ -53,7 +53,7 @@ class FrontController extends Controller
     public function urlParser()
     {
         if (isset ($_GET['side'])) {
-            $this->side = $_GET['side'];
+            $this->side = filter_input(INPUT_POST, 'side');
         } else {
             $this->side = 'public';
         }

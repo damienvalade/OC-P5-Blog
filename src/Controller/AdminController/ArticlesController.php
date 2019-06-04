9b2fd1a5cd52e3cf2971ea0b21d82925312cda87
damienvalade <damien@damienvalade.fr>
2019-06-04 12:26:38 +0200

    update for codacy
    update for codacy

--- src/Controller/AdminController/ArticlesController.php
+++ src/Controller/AdminController/ArticlesController.php
@@ -26,24 +26,25 @@ class ArticlesController extends FrontController
 
     public function indexAction()
     {
-        $this->data = $this->database->innerJoin('','','');
+        $this->data = $this->database->innerJoin('', '', '');
 
         return ['articles' => $this->data];
     }
 
-    public function updateAction(){
+    public function updateAction()
+    {
 
-        $id = (int)$_GET['id'];
+        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
         if (!empty($_POST)) {
 
-            if (!empty($_POST['inputName']) && !empty($_POST['inputChapo'])
-                && !empty($_POST['inputType']) && !empty($_POST['inputContenue'])) {
+            $inputName = filter_input(INPUT_POST, 'inputName', FILTER_SANITIZE_STRING);
+            $inputChapo = filter_input(INPUT_POST, 'inputChapo', FILTER_SANITIZE_STRING);
+            $inputType = filter_input(INPUT_POST, 'inputType', FILTER_SANITIZE_STRING);
+            $inputContenue = filter_input(INPUT_POST, 'inputContenue', FILTER_SANITIZE_SPECIAL_CHARS);
 
-                $inputName = $_POST['inputName'];
-                $inputChapo = $_POST['inputChapo'];
-                $inputType = $_POST['inputType'];
-                $inputContenue = $_POST['inputContenue'];
+            if ($inputName !== null && $inputChapo !== null
+                && $inputType !== null && $inputContenue !== null) {
 
                 $filename = $this->upload('photoarticle');
 
@@ -57,7 +58,7 @@ class ArticlesController extends FrontController
                     'dateCreation' => date("Y-m-d H:i:s")
                 ];
 
-                $this->database->update('articles',$id, $data ,'id' );
+                $this->database->update('articles', $id, $data, 'id');
 
                 $this->session->setValidate('inscription', 'Article mis à jour');
             }
@@ -67,41 +68,42 @@ class ArticlesController extends FrontController
         $this->data2 = $this->database->read('categories');
 
         return ['articles' => $this->data,
-            'types' => $this->data2] ;
+            'types' => $this->data2];
     }
 
-    public function createAction(){
+    public function createAction()
+    {
 
         $this->data2 = $this->database->read('categories');
 
         if (!empty($_POST)) {
 
-            if (!empty($_POST['inputName']) && !empty($_POST['inputChapo'])
-                && !empty($_POST['inputType']) && !empty($_POST['inputContenue'])) {
+            $inputName = filter_input(INPUT_POST, 'inputName', FILTER_SANITIZE_STRING);
+            $inputChapo = filter_input(INPUT_POST, 'inputChapo', FILTER_SANITIZE_STRING);
+            $inputType = filter_input(INPUT_POST, 'inputType', FILTER_SANITIZE_STRING);
+            $inputContenue = filter_input(INPUT_POST, 'inputContenue', FILTER_SANITIZE_SPECIAL_CHARS);
 
-                $inputName = $_POST['inputName'];
-                $inputChapo = $_POST['inputChapo'];
-                $inputType = $_POST['inputType'];
-                $inputContenue = $_POST['inputContenue'];
+            if ($inputName !== null && $inputChapo !== null
+                && $inputType !== null && $inputContenue !== null) {
 
-                    $filename = $this->upload('photoarticle');
+                $filename = $this->upload('photoarticle');
 
-                        $data = [
-                            'nomArticle' => $inputName,
-                            'chapoArticle' => $inputChapo,
-                            'auteurArticle' => $this->session->userName(),
-                            'id_categories' => $inputType,
-                            'contenueArticle' => $inputContenue,
-                            'image' => 'img\\\\photoarticle\\\\' . $filename,
-                            'dateCreation' => date("Y-m-d H:i:s")
-                        ];
+                $data = [
+                    'nomArticle' => $inputName,
+                    'chapoArticle' => $inputChapo,
+                    'auteurArticle' => $this->session->userName(),
+                    'id_categories' => $inputType,
+                    'contenueArticle' => $inputContenue,
+                    'image' => 'img\\\\photoarticle\\\\' . $filename,
+                    'dateCreation' => date('Y-m-d H:i:s')
+                ];
 
-                        $this->database->create('articles', $data);
+                $this->database->create('articles', $data);
 
-                        $this->session->setValidate('inscription', 'Article mis en ligne');
+                $this->session->setValidate('inscription', 'Article mis en ligne');
             }
         }
-        return ['types' => $this->data2] ;
+        return ['types' => $this->data2];
     }
 
 }

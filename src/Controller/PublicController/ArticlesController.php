2febcfd1b83e3be87a48466e379bef18a4638779
damienvalade <damien@damienvalade.fr>
2019-06-05 17:59:12 +0200

    update view articles
    update view articlesa

--- src/Controller/PublicController/ArticlesController.php
+++ src/Controller/PublicController/ArticlesController.php
@@ -8,6 +8,7 @@ use Core\Model\Model;
 class ArticlesController extends FrontController
 {
     protected $data;
+    protected $data2;
     protected $database;
 
     public function __construct()
@@ -19,9 +20,25 @@ class ArticlesController extends FrontController
     {
         $this->data = $this->database->read('articles');
 
+        $this->data2 = $this->database->read('categories');
+
         $response = [ 'path' => 'PublicView/Pages/articles.twig',
-            'data' => ['articles' => $this->data],
+            'data' => ['articles' => $this->data,
+                       'categories' => $this->data2],
             ];
+
+        return $response;
+    }
+
+    public function viewAction(){
+
+        $id_article = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+        $this->data = $this->database->read('articles', $id_article, 'id', true);
+
+        $response = [ 'path' => 'PublicView/Pages/viewArticles.twig',
+            'data' => ['articles' => $this->data],
+        ];
 
         return $response;
     }

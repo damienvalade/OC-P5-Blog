a33e07ad0776c2dc24c9e6eeeed0fac289617db9
damienvalade <damien@damienvalade.fr>
2019-06-12 18:26:23 +0200

    update index.php
    update index.php

--- public/index.php
+++ public/index.php
@@ -1,16 +1,12 @@
 <?php
 
-define('ROOT', dirname(__DIR__));
-
-require_once ROOT . '/vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
 use Core\Controller\FrontController;
 use Tracy\Debugger;
 
 Debugger::enable();
 Debugger::$strictMode = true;
-
-$coockies = new Core\Controller\Cookies\Cookies();
 
 $test = new FrontController();
 

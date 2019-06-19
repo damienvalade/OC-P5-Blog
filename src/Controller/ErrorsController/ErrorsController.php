87a3f8d726d0244e30777d67ddd3bb8614a0ec2b
damienvalade <damien@damienvalade.fr>
2019-06-20 00:19:37 +0200

    update redirect errors
    update redirect errors

---
+++ src/Controller/ErrorsController/ErrorsController.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Controller\ErrorsController;
+
+use Core\Controller\FrontController;
+
+class ErrorsController extends FrontController
+{
+    public function errorsAction(){
+
+        $error = filter_input(INPUT_GET,'type',FILTER_SANITIZE_NUMBER_INT);
+
+        switch ($error) {
+            case 401:
+                $error = $this->unauthorized();
+                break;
+            case 403:
+                $error = $this->forbidden();
+                break;
+            case 404:
+                $error = $this->notfound();
+                break;
+            case 500:
+                 $error = $this->serverError();
+                break;
+        }
+
+        $response = [ 'path' => $error,
+            'data' => [],
+        ];
+
+        return $response;
+    }
+}

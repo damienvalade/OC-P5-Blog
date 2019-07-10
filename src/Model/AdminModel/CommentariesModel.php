eb5b3f51d7d2d7d61844598f1b295a7520ef5489
damienvalade <damien@damienvalade.fr>
2019-07-11 00:37:12 +0200

    Update commentaries validation
    Update commentaries validation

--- src/Model/AdminModel/CommentariesModel.php
+++ src/Model/AdminModel/CommentariesModel.php
@@ -17,7 +17,7 @@ class CommentariesModel extends Model
      */
     public function innerJoin()
     {
-        $querry = 'SELECT articles.nom_article, commentaire.commentaire, users.firstname, commentaire.date_creation, commentaire.id, commentaire.id_article
+        $querry = 'SELECT articles.nom_article, commentaire.commentaire, commentaire.is_valide, users.firstname, commentaire.date_creation, commentaire.id, commentaire.id_article
                     FROM commentaire
                     JOIN articles ON commentaire.id_article = articles.id
                     JOIN users ON commentaire.id_auteur = users.id';
